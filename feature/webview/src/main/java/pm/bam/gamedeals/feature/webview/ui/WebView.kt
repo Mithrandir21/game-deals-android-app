bd2f563fb62aecbca72f96b6cfcaa2120d6a358d
Bahram Malaekeh <Mithrandir21@users.noreply.github.com>
2024-04-10 15:08:25 +0100

    Simplified browsing to external browser

--- feature/webview/src/main/java/pm/bam/gamedeals/feature/webview/ui/WebView.kt
+++ feature/webview/src/main/java/pm/bam/gamedeals/feature/webview/ui/WebView.kt
@@ -9,11 +9,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -30,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +43,6 @@ internal fun WebView(
     url: String,
     onBack: () -> Unit
 ) {
-    var showMenu by rememberSaveable { mutableStateOf(false) }
     var loading by rememberSaveable { mutableStateOf(true) }
     val uriHandler = LocalUriHandler.current
 
@@ -72,25 +68,10 @@ internal fun WebView(
                             if (loading) {
                                 CircularProgressIndicator()
                             }
-                            IconButton(onClick = { showMenu = !showMenu }) {
+                            IconButton(onClick = { uriHandler.openUri(url) }) {
                                 Icon(
-                                    imageVector = Icons.Filled.MoreVert,
-                                    contentDescription = stringResource(id = R.string.webview_screen_navigation_toolbar_overflow)
-                                )
-                            }
-                            DropdownMenu(
-                                expanded = showMenu,
-                                onDismissRequest = { showMenu = false }
-                            ) {
-                                DropdownMenuItem(
-                                    text = { Text(text = stringResource(id = R.string.webview_screen_navigation_open_in_browser)) },
-                                    leadingIcon = {
-                                        Icon(
-                                            imageVector = Icons.Filled.Share,
-                                            contentDescription = stringResource(id = R.string.webview_screen_navigation_open_in_browser)
-                                        )
-                                    },
-                                    onClick = { uriHandler.openUri(url) }
+                                    painter = painterResource(id = R.drawable.browser),
+                                    contentDescription = stringResource(id = R.string.webview_screen_navigation_open_in_browser)
                                 )
                             }
                         }
