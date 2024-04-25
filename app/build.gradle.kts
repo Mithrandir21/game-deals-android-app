27d1b443212ea48f9a733793d14570ccaaccb73b
Bahram Malaekeh <Mithrandir21@users.noreply.github.com>
2024-04-25 09:27:47 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,8 +64,8 @@ android {
         applicationId = "pm.bam.gamedeals"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.5"
+        versionCode = 9
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
